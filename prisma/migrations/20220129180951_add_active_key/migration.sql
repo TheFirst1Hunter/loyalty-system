c660d11724e95f5bc1b6485d57c2ab784babdfb2
abdullaa mac <abdullaa.taha.at@gmail.com>
2022-01-29 22:49:17 +0300

    fix: added isActive key

---
+++ prisma/migrations/20220129180951_add_active_key/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Costumer" ADD COLUMN     "active" BOOLEAN NOT NULL DEFAULT true;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "active" BOOLEAN NOT NULL DEFAULT true;
