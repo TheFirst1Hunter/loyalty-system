633a8778c30909aa57444c3dd0844da7b2dc37ec
abdullaa mac <abdullaa.taha.at@gmail.com>
2022-01-30 17:09:42 +0300

    feat: sort closest birthdaies

--- src/costumers/costumer.helpers.ts
+++ src/costumers/costumer.helpers.ts
@@ -1,6 +1,26 @@
 import { genSalt, hash } from 'bcrypt';
+import { Costumer } from './entities/costumer.entity';
 
 export const hashPassword = async (text: string) => {
   const salt = await genSalt(12);
   return await hash(text, salt);
+};
+
+export const sortByBirthday = (data: Costumer[], date: Date = new Date()) => {
+  const thisMonth = date.getMonth();
+
+  const thisDay = date.getDate();
+
+  const ddd = [];
+
+  data.forEach((d) => {
+    if (
+      d.birthDate.getMonth() > thisMonth ||
+      (d.birthDate.getMonth() == thisMonth && d.birthDate.getDate() >= thisDay)
+    ) {
+      ddd.push(d);
+    }
+  });
+
+  return ddd;
 };
