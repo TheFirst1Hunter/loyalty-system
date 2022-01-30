bb850265e7d54b41732b8ae994aac0528b3da65f
abdullaa mac <abdullaa.taha.at@gmail.com>
2022-01-30 18:37:41 +0300

    feat: search by name and serial

--- src/costumers/dto/filter-costumer.dto.ts
+++ src/costumers/dto/filter-costumer.dto.ts
@@ -1,33 +1,41 @@
-import { Transform, Type } from 'class-transformer';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { Transform } from 'class-transformer';
+import { IsString, IsOptional, isNumberString } from 'class-validator';
 
 export class QueryCostumerDto {
-  @Type(() => Number)
   @Transform(({ value, key, obj }) => {
     obj[key] = Number(value) || 0;
   })
   skip = 0;
 
-  @Type(() => Number)
   @Transform(({ value, key, obj }) => {
     obj[key] = Number(value) || 5;
   })
   take = 5;
 
-  @Type(() => Boolean)
   @Transform(({ value, key, obj }) => {
     obj[key] = Boolean(value) || false;
   })
   nearestBirthday = false;
 
-  @Type(() => Date)
   @Transform(({ value, key, obj }) => {
     obj[key] = new Date(value) || new Date();
   })
   dateMin: Date;
 
-  @Type(() => Date)
   @Transform(({ value, key, obj }) => {
     obj[key] = new Date(value) || new Date();
   })
   dateMax: Date;
+
+  @IsString()
+  @IsOptional()
+  name: string;
+
+  @Transform(({ value, key, obj }) => {
+    if (isNumberString(value)) obj[key] = Number(value);
+    else
+      throw new HttpException('please enter a number', HttpStatus.BAD_REQUEST);
+  })
+  serial: number;
 }
