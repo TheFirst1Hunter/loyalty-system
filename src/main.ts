845a18e57710ab98b9feb9b9c9173c4b20cca29c
abdullaa mac <abdullaa.taha.at@gmail.com>
2022-02-01 15:58:53 +0300

    fix: cors err

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import cors from 'cors';
 import { AppModule } from './app.module';
 import { PORT } from './utils/secrets';
 
 async function bootstrap() {
-  console.debug(PORT);
   const app = await NestFactory.create(AppModule);
+
+  app.use(cors({ origin: '*' }));
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(PORT);
 }
 
