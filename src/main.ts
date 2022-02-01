6ba67dd5e6f87e6b8c6212d1a03b3ed7eab0a37b
abdullaa mac <abdullaa.taha.at@gmail.com>
2022-02-01 13:52:22 +0300

    fix: passing unwated feilds

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { PORT } from './utils/secrets';
 async function bootstrap() {
   console.debug(PORT);
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(PORT);
 }
 
