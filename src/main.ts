4a92964d6bf112f493fc13a2e5ed0715f310413d
abdullaa mac <abdullaa.taha.at@gmail.com>
2022-02-02 09:32:23 +0300

    chore: removed console.log

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 import { PORT } from './utils/secrets';
 
 async function bootstrap() {
-  console.debug(PORT);
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(PORT);
