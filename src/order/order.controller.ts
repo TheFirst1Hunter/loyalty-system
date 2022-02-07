22335b9c85ad850ed4fc3a449cdc98de85f9d0bb
abdullaa mac <abdullaa.taha.at@gmail.com>
2022-02-07 15:05:03 +0300

    fix: unfied response

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -12,6 +12,7 @@ import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { QueryOrderDto } from './dto/filter-order.dto';
+import { ResponseShape } from '../utils';
 
 @Controller('order')
 export class OrderController {
@@ -19,17 +20,23 @@ export class OrderController {
 
   @Post()
   async create(@Body() createOrderDto: CreateOrderDto) {
-    return await this.orderService.create(createOrderDto);
+    const data = await this.orderService.create(createOrderDto);
+
+    return new ResponseShape(true, data);
   }
 
   @Get()
   async findAll(@Query() query: QueryOrderDto) {
-    return await this.orderService.findAll(query);
+    const data = await this.orderService.findAll(query);
+
+    return new ResponseShape(true, data);
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
-    return await this.orderService.findOne(id);
+    const data = await this.orderService.findOne(id);
+
+    return new ResponseShape(true, data);
   }
 
   @Put(':id')
@@ -37,7 +44,9 @@ export class OrderController {
     @Param('id') id: string,
     @Body() updateOrderDto: UpdateOrderDto,
   ) {
-    return await this.orderService.update(id, updateOrderDto);
+    const data = await this.orderService.update(id, updateOrderDto);
+
+    return new ResponseShape(true, data);
   }
 
   @Delete(':id')
