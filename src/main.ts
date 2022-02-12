e29d04d97cc0700f9d52ac830a38203d4b80bb0c
abdullaa mac <abdullaa.taha.at@gmail.com>
2022-02-12 16:29:01 +0300

    feat: prisma err handler

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import cors from 'cors';
 import { AppModule } from './app.module';
 import { PORT } from './utils/secrets';
+import { PrismaErrHandler } from './utils/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,8 @@ async function bootstrap() {
   app.use(cors({ origin: '*' }));
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.useGlobalFilters(new PrismaErrHandler());
 
   await app.listen(PORT);
 }
