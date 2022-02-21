bb096c9169826b495f9a714eabb0df1d5a1d4696
abdullaa mac <abdullaa.taha.at@gmail.com>
2022-02-21 10:11:35 +0300

    fix: expired token

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 rm package-lock.json
 
-git pull https://thefirst1hunter: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@github.com/thefirst1hunter/loyalty-system.git dev
+git pull https://thefirst1hunter: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@github.com/thefirst1hunter/loyalty-system.git dev
 
 npm i
 
