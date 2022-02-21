9735b5107dc2cc35b5f3165c223e3757bbb26b58
abdullaa mac <abdullaa.taha.at@gmail.com>
2022-02-21 12:21:17 +0300

    fix: separated the "income" from the "order" in the statistics

--- src/statistics/statistics.service.ts
+++ src/statistics/statistics.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, HttpStatus, HttpException, Inject } from '@nestjs/common';
 import { isDate } from 'class-validator';
 import { QueryStatisticsDto } from './dto/filter-statistics.dto';
-import { Statistic } from './entities/statistic.entity';
+import { StatisticIncome, StatisticOrder } from './entities/statistic.entity';
 import { getDaysInBetween, incrementDate } from '../utils';
 import { globalProviders } from '../globals/global.types';
 import { PrismaClient } from '@prisma/client';
@@ -13,7 +13,60 @@ export class StatisticsService {
     //
   }
 
-  async findAll(filter: QueryStatisticsDto): Promise<Statistic> {
+  async getOrders(filter: QueryStatisticsDto): Promise<StatisticOrder> {
+    // Destruct the from-to date string
+    const { from, to } = filter;
+
+    // Check if it's a valid values
+    if (!isDate(new Date(from))) {
+      throw new HttpException(
+        '"from" must be a valid string',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (!isDate(new Date(to))) {
+      throw new HttpException(
+        '"to" must be a valid string',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    // Convert the date string to Date object
+    const fromDate = new Date(from);
+    const toDate = new Date(to);
+
+    // Calculate the days in between the dates to determine the "loop" duration
+    const daysInBetween = getDaysInBetween(fromDate, toDate);
+
+    // Initial query
+    let orderCountQueryTotal = `select (select count(*) from "Order") as "allOrders"`;
+    let ordersWithCreditQuery = `select (select count(*) from "Order" where not  "creditUsed" = 0) as "allOrders"`;
+
+    for (let index = 0; index < daysInBetween; index++) {
+      const fromFiled = incrementDate(fromDate, 1);
+
+      const fromFiledCopy = new Date(fromFiled);
+
+      const toFiled = incrementDate(fromFiledCopy, 1);
+
+      // Manipulate the query
+      orderCountQueryTotal += `,(select count(*) from "Order" where date between '${fromFiled}' and '${toFiled}') as"${fromFiled}"`;
+      ordersWithCreditQuery += `,(select count(*) from "Order" where not  "creditUsed" = 0 and date between '${fromFiled}' and '${toFiled}') as"${fromFiled}"`;
+    }
+
+    // Query the daily income
+    const orderCount = (
+      await this.prisma.$queryRawUnsafe(orderCountQueryTotal)
+    )[0];
+    const orderCountWithCredit = (
+      await this.prisma.$queryRawUnsafe(ordersWithCreditQuery)
+    )[0];
+
+    return { orderCount, orderCountWithCredit };
+  }
+
+  async getIncome(filter: QueryStatisticsDto): Promise<StatisticIncome> {
     // Destruct the from-to date string
     const { from, to } = filter;
 
@@ -41,8 +94,6 @@ export class StatisticsService {
 
     // Initial query
     let incomeQuery = `select (select sum("totalPrice") from "Order" where date between '${from}' and '${to}') as "DailyIncome"`;
-    let orderCountQueryTotal = `select (select count(*) from "Order") as "allOrders"`;
-    let ordersWithCreditQuery = `select (select count(*) from "Order" where not  "creditUsed" = 0) as "allOrders"`;
 
     for (let index = 0; index < daysInBetween; index++) {
       const fromFiled = incrementDate(fromDate, 1);
@@ -53,19 +104,11 @@ export class StatisticsService {
 
       // Manipulate the query
       incomeQuery += `,(select sum("totalPrice") from "Order" where date between '${fromFiled}' and '${toFiled}') as "${fromFiled}"`;
-      orderCountQueryTotal += `,(select count(*) from "Order" where date between '${fromFiled}' and '${toFiled}') as"${fromFiled}"`;
-      ordersWithCreditQuery += `,(select count(*) from "Order" where not  "creditUsed" = 0 and date between '${fromFiled}' and '${toFiled}') as"${fromFiled}"`;
     }
 
     // Query the daily income
     const dailyIncome = (await this.prisma.$queryRawUnsafe(incomeQuery))[0];
-    const orderCount = (
-      await this.prisma.$queryRawUnsafe(orderCountQueryTotal)
-    )[0];
-    const orderCountWithCredit = (
-      await this.prisma.$queryRawUnsafe(ordersWithCreditQuery)
-    )[0];
 
-    return { dailyIncome, orderCount, orderCountWithCredit };
+    return { dailyIncome };
   }
 }
