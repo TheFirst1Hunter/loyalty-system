bffdeeb6534589cb64654ad4487d70ee5e05ea4f
abdullaa mac <abdullaa.taha.at@gmail.com>
2022-02-28 15:16:24 +0300

    fix seed

--- prisma/data/costumer.ts
+++ prisma/data/costumer.ts
@@ -1,9 +1,11 @@
 import faker from '@faker-js/faker';
 import { v4 as uuid4 } from 'uuid';
+import { BDstatus } from '@prisma/client';
 import { prisma } from '..';
 
 export const costumers = async (): Promise<string[]> => {
   const ids = [];
+  const status = ['today', 'tomorrow', 'nextMonth', 'nothing'];
   for (let index = 0; index < 100; index++) {
     const costumer = await prisma.costumer.create({
       data: {
@@ -14,7 +16,9 @@ export const costumers = async (): Promise<string[]> => {
         UID: uuid4(),
         active: true,
         birthDate: faker.date.past(),
-        // isHisBirthday: parseInt(`${Math.random()}`) % 2 == 0,
+        birthdayStatus: status[
+          parseInt(`${Math.random()}`) % status.length
+        ] as BDstatus,
       },
     });
 
