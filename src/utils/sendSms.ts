701025a9e71693f86ce3a5b01fff5752a0036c71
abdullaa mac <abdullaa.taha.at@gmail.com>
2022-03-26 15:08:44 +0300

    fix: acceindetly hardcoded the "to"

--- src/utils/sendSms.ts
+++ src/utils/sendSms.ts
@@ -3,7 +3,7 @@ import { twilio } from './clients';
 export default async function sendSms(to: string, body: string) {
   const msgData = {
     from: process.env.TWILIO_PHONE_NUMBER,
-    to: '+9647707757022',
+    to,
     body,
   };
 
