1b7fcf0ebda928897e2d5b3de234041419e26ba4
abdullaa mac <abdullaa.taha.at@gmail.com>
2022-05-24 16:55:13 +0300

    changed the github token

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 rm package-lock.json
 
-git pull https://thefirst1hunter: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@github.com/thefirst1hunter/loyalty-system.git dev
+git pull https://thefirst1hunter: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@github.com/thefirst1hunter/loyalty-system.git dev
 
 npm i
 
